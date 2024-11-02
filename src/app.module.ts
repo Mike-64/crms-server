8334036f01db133d4751ff90663ff0d30c6b831e
Dave McKay <dave@promulgareconsulting.com>
2024-11-02 15:28:46 -0400

    added .env and created operations module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { MessageModule } from './message/message.module';
 import { LedgerModule } from './ledger/ledger.module';
 import { ConnectionsModule } from './connections/connections.module';
 import { ParserModule } from './parser/parser.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Templates } from './tables/templates.entity';
 import { Instance } from './tables/instance.entity';
 import { Oid4vcController } from './oid4vc/oid4vc.controller';
@@ -16,6 +16,8 @@ import { Oid4vcModule } from './oid4vc/oid4vc.module';
 import { Oid4vcService } from "./oid4vc/oid4vc.service";
 import { JobsModule } from './jobs/jobs.module';
 import { Job } from './jobs/entities/job.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { OperationsModule } from './operations/operations.module';
 
 @Module({
   imports: [
@@ -26,19 +28,25 @@ import { Job } from './jobs/entities/job.entity';
     LedgerModule,
     ConnectionsModule,
     ParserModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      entities: [Templates,Instance,Job],
-      database: 'postgres',
-      synchronize: true,
-      logging: true,
-    }),
     Oid4vcModule,
     JobsModule,
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],      
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+          port: Number(configService.get<string>('DATABASE_PORT')),
+          username: configService.get<string>('DATABASE_USER'),
+          password: configService.get<string>('DATABASE_PASS'),
+          database: configService.get<string>('DATABASE_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          type: 'postgres' as 'postgres',
+          host: configService.get('DATABASE_HOST'),
+          synchronize: true,
+          logging: true,
+      }),
+    }),
+    OperationsModule,
   ],
   controllers: [AppController, Oid4vcController],
   providers: [AppService, Oid4vcService],
